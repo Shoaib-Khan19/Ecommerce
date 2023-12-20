e08fce43629ac71153460ed12a1f46bf5b77d1bb
Shoaib-Khan19 <shoaibkhanyd19@gmail.com>
2023-12-20 18:38:45 +0500

    get

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());// calling middleware json() helps to read/write json code 
 
+app.get("/", (req, res) =>{
+    res.send("Hello from api")
+});
 /*Product Routes*/ 
 const product = require("./routes/productRoutes");
 app.use('/api/v1',product);
